98bc5102ebef3ec35900c49e407811ca461feea4
Thomas Chemin <tchemin@z4r5p7.42lyon.fr>
2026-01-07 17:09:54 +0100

    mod4 finished

---
+++ module04/ex2/ft_stream_management.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def main():
+    print("=== CYBER ARCHIVES - COMMUNICATION SYSTEM ===\n")
+    try:
+        id = input("Input Stream active. Enter archivist ID: ")
+        status = input("Input Stream active. Enter status report: ")
+        print("")
+        sys.stdout.write(f"[STANDARD] Archive status from {id}: {status}\n")
+        sys.stderr.write("[ALERT] System diagnostic: Communication " +
+                         "channels verified\n")
+        sys.stdout.write("[STANDARD] Data transmission complete\n")
+        print("\nThree-channel communication test successful.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ module04/ex3/ft_vault_security.py
@@ -0,0 +1,23 @@
+def main():
+    print("=== CYBER ARCHIVES - VAULT SECURITY SYSTEM ===\n")
+
+    try:
+        print("Initiating secure vault access...")
+        with open("classified_data.txt", "r") as file:
+            print("Vault connection established with failsafe protocols\n")
+            print("SECURE EXTRACTION:")
+            for line in file:
+                print(line, end="")
+        with open("security_protocols.txt", "w") as file:
+            print("\n\nSECURE PRESERVATION:")
+            security = "[CLASSIFIED] New security protocols archived"
+            print(security)
+            file.write(security)
+            print("Vault automatically sealed upon completion\n")
+        print("All vault operations completed with maximum security.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    main()
